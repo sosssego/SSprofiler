19512093d9d8e1883d4ad0b977a2abad25a791fa
dell\sosssego <fredmoron@gmail.com>
2019-07-15 01:21:19 +0100

    big refactor

--- source/frdProfile.cpp
+++ source/frdProfile.cpp
@@ -11,29 +11,31 @@ namespace frd {
 		startTimer(0);
 	}
 	int Profiler::addNewTimer(string name) {
-		long long temp;
-		temp = 0;
-		total_time.push_back(temp);
-		temp = chrono::duration_cast<chrono::nanoseconds> (chrono::steady_clock::now().time_since_epoch()).count();
+		total_time.push_back(std::chrono::duration < int64_t>(0));
+		std::chrono::time_point<std::chrono::high_resolution_clock> temp = std::chrono::high_resolution_clock::now();
 		last_time.push_back(temp);
 		names.push_back(name);
 		return int(total_time.size()) - 1;
 	}
 	void Profiler::startTimer(int i) {
-		last_time[i] = chrono::duration_cast<chrono::nanoseconds> (chrono::steady_clock::now().time_since_epoch()).count();
+		last_time[i] = std::chrono::high_resolution_clock::now();
 	}
 	void Profiler::addPartialTime(int i) {
-		current_time = chrono::duration_cast<chrono::nanoseconds> (chrono::steady_clock::now().time_since_epoch()).count();
+		auto current_time = std::chrono::high_resolution_clock::now();
 		total_time[i] += current_time - last_time[i];
 		last_time[i] = current_time;
 	}
 	void Profiler::clearStoredTime(int i) {
-		total_time[i] = 0;
+		total_time[i] = 0ms;
 	}
 	void Profiler::printTimeSummary() {
 		addPartialTime(0);
 		for (int i = 0; i < (int)names.size(); ++i) {
-			printf("%15s\t%12lld\t\tcounts, %.6fsec,\t%.6f\t%% \n", names[i].c_str(), total_time[i], double(total_time[i]) * 0.000000001, 100 * (float)(total_time[i]) / (float)total_time[0]);
+			printf("%15s\t%.12fsec,\t%.6f\t%% \n",
+				names[i].c_str(),
+				total_time[i].count(),
+				//double(total_time[i].count()) * 0.000000001,
+				100 * (float)(total_time[i].count()) / (float)total_time[0].count());
 
 		}
 	}
--- source/frdProfile.hpp
+++ source/frdProfile.hpp
@@ -2,14 +2,16 @@
 #include <chrono>
 #include <vector>
 #include <string>
+#include <cstdint>
 
 namespace frd {
 	class Profiler
 	{
 	public:
-		std::vector<long long> total_time, last_time;
+		std::vector<std::chrono::duration<double>> total_time;
+		std::vector<std::chrono::time_point<std::chrono::high_resolution_clock>> last_time;
 		std::vector<std::string> names;
-		long long current_time;
+		//std::chrono::time_point<int64_t> current_time;
 
 
 		int addNewTimer(std::string name);
